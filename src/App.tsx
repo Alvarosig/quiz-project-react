0431243bd0b002f59b5c08d9db50a839f4cc7b9e
Álvaro Silva Garcia <alvaro_s.g@hotmail.com>
2024-01-18 19:20:00 -0300

    Add maping the data to elements and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import './App.css'
 import Question from './components/Question'
 import Quizzical from './components/Quizzical';
 import { useState } from "react"
+import { QuestionData } from "./data/IData.ts"
+import { mock } from './data/data.ts';
+import CheckButton from './components/CheckButton/index.tsx';
 
 function App() {
 
   const [quizStarted, setQuizStarted] = useState(false);
+  const [showAnswer, setShowAnswer] = useState(false);
 
   function startQuiz() {
     setQuizStarted(true);
+  }
+
+  function handleShowAnwser() {
+    setShowAnswer(true)
   }
 
   return (
@@ -16,14 +24,36 @@ function App() {
       {!quizStarted && <Quizzical startQuiz={startQuiz} />}
 
       {quizStarted && (
-      <div className="question_container">
-        <Question />
-        <div className='divider'></div>
-        <Question />
-        <div className='divider'></div>
-        <Question />
-        <div className='divider'></div>
-      </div>
+        <div>
+          <div style={{
+            display: "flex",
+            margin: "0 auto",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+
+            <div className="question_container">
+              {mock.results.map((data: QuestionData, index: number) => {
+                return (
+                  <>
+                    <Question
+                      key={index}
+                      question={data.question}
+                      correct={data.correct_answer}
+                      incorrect={data.incorrect_answers}
+                      showAnswer={showAnswer}
+                    />
+                    <div className='divider'></div>
+                  </>
+                );
+              })}
+            </div>
+
+          </div>
+
+          <CheckButton handleShowAnswer={handleShowAnwser}/>
+
+        </div>
       )}
     </>
   )
--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,15 +1,49 @@
+import { shuffle as LoShuffle } from "lodash";
 import style from "./index.module.css"
+import { useMemo, useState } from "react";
 
-export default function Question() {
+interface QuestionProps {
+    question: string;
+    correct: string;
+    incorrect: string[];
+    showAnswer: boolean;
+}
+
+export default function Question({ question, correct, incorrect, showAnswer }: QuestionProps) {
+
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const options = useMemo(() => LoShuffle([correct, ...incorrect]), [correct, incorrect])
+
     return (
-        <div className='question'>
-            <span className={style.title}>How would one say goodbye in Spanish?</span>
-            <div className={style.options}>
-                <button className={`${style.option} ${style.selected}`}>Adiós</button>
-                <button className={style.option}>Hola</button>
-                <button className={style.option}>Au Revoir</button>
-                <button className={style.option}>Salir</button>
+        <>
+            <div className='question'>
+                <span className={style.title} dangerouslySetInnerHTML={{ __html: question }}></span>
+                <div className={`${style.options} ${showAnswer ? style.disable_hover : ""}`}>
+                    {options.map((option, index) => (
+                        showAnswer ? (
+                            <button
+                                key={index}
+                                disabled={true}
+                                className=
+                                {`${style.option} 
+                                ${correct === option ? style.correct : ""} 
+                                ${selectedOption === option && incorrect.includes(option) ? style.incorrect : ""}`}
+                            >
+                                {option}
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() => setSelectedOption(option)}
+                                key={index}
+                                className=
+                                {`${style.option} ${option === selectedOption ? style.selected : ""}`}
+                            >
+                                {option}
+                            </button>
+                        )
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
